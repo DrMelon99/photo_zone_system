59f4f9583a71d602ff172d0572f2282b99c1ba2d
Hitbee-dev <ckim.esw@gmail.com>
2023-01-16 17:35:45 +0900

    :fire: Remove: Reciver Code & Detection number

--- linux/deepface/fr_test.py
+++ linux/deepface/fr_test.py
@@ -1,6 +1,14 @@
 from deepface import DeepFace
 import cv2
-import json
+import socket
+from _thread import *
+
+HOST_IP = '192.168.0.29'
+HOST_PORT = 8995
+
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket.connect((HOST_IP, HOST_PORT))
+client_socket.sendall('face-recognition'.encode())
 
 image_path = "linux/deepface/image.jpg"
 webcam = cv2.VideoCapture(0)
@@ -15,13 +23,15 @@ while webcam.isOpened():
     if status:
         cv2.imwrite(image_path, frame)
         demography = DeepFace.analyze(image_path)
-        print(demography)
+        send_data = f"{demography['dominant_emotion']} {demography['age']} {demography['dominant_gender']}"
+        client_socket.send(send_data.encode())
+        print(send_data)
         x, y, w, h = demography['region'].values()
-        print(x, y, w, h)
+        # print(x, y, w, h)
 
         if x != 0 and y != 0:
             cv2.rectangle(frame, [x, y], [x + w, y + h], (0, 0, 255), 3)
-        cv2.imshow("test", frame)
+        cv2.imshow("image", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
